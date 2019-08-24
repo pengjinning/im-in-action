76e713ae26c969d9950e7efcb7aab7dd32966c01
jackning <270580156@qq.com>
2019-08-24 10:23:46 +0800

    update

--- im-in-action-tutorial/netty-websocket/src/main/java/io/xiaper/ChatServer.java
+++ im-in-action-tutorial/netty-websocket/src/main/java/io/xiaper/ChatServer.java
@@ -20,6 +20,9 @@ import java.net.InetSocketAddress;
  */
 public class ChatServer {
 
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "3443" : "3080"));
+
     // 创建 DefaultChannelGroup，其将保存所有已经连接的 WebSocket Channel
     private final ChannelGroup channelGroup = new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
     private final EventLoopGroup group = new NioEventLoopGroup();
@@ -33,6 +36,10 @@ public class ChatServer {
                 .childHandler(createInitializer(channelGroup));
         //
         ChannelFuture future = bootstrap.bind(address);
+
+        System.out.println("Open your web browser and navigate to " +
+                (SSL? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
         future.syncUninterruptibly();
         channel = future.channel();
         //
@@ -67,10 +74,10 @@ public class ChatServer {
 //        }
 //         Integer.parseInt(args[0]);
 
-        int port = 8019;
+//        int port = 8019;
         final ChatServer endpoint = new ChatServer();
         //
-        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+        ChannelFuture future = endpoint.start(new InetSocketAddress(PORT));
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
@@ -78,5 +85,6 @@ public class ChatServer {
             }
         });
         future.channel().closeFuture().syncUninterruptibly();
+
     }
 }
