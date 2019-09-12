3a9f3685e971ae128476d6c46838420ec66c1931
jackning <270580156@qq.com>
2019-09-12 11:20:02 +0800

    update

--- im-in-action-tutorial/spring-boot-dwr/src/main/java/io/xiaper/dwr/DWRApplication.java
+++ im-in-action-tutorial/spring-boot-dwr/src/main/java/io/xiaper/dwr/DWRApplication.java
@@ -10,9 +10,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
-public class Application {
+public class DWRApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(DWRApplication.class, args);
     }
 
     @Bean
