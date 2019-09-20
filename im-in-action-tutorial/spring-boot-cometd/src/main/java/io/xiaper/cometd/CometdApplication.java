2f72b65ffd502c1dfb7eec0e3148073291c5aa2c
jackning <270580156@qq.com>
2019-09-21 06:42:27 +0800

    update

---
+++ im-in-action-tutorial/spring-boot-cometd/src/main/java/io/xiaper/cometd/CometdApplication.java
@@ -0,0 +1,13 @@
+package io.xiaper.cometd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CometdApplication.class, args);
+	}
+
+}
