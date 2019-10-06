ff7297405ce5769068470a908924c00ce62b034f
jackning <270580156@qq.com>
2019-10-06 10:14:39 +0800

    update

--- im-in-action-tutorial/mongo-chat2019/src/main/java/com/example/chatAppDemo/chatAppDemo/repos/ChatChannelRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatAppDemo.chatAppDemo.repos;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.chatAppDemo.chatAppDemo.models.Channel;
-
-public interface ChatChannelRepo extends ReactiveMongoRepository<Channel, String>{
-
-}
