6508e0e62dbdc2267b650a74d2d018989e94cc7b
jackning <270580156@qq.com>
2019-10-10 08:22:27 +0800

    update

--- im-in-action-tutorial/spring-boot-polling/src/main/java/io/xiaper/polling/controller/RouteController.java
+++ im-in-action-tutorial/spring-boot-polling/src/main/java/io/xiaper/polling/controller/RouteController.java
@@ -20,6 +20,7 @@ package io.xiaper.polling.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -27,7 +28,9 @@ import java.security.Principal;
 public class RouteController {
 
     @GetMapping("/")
-    public String index(Principal principal, Model model) {
+    public String index(Principal principal, Model model, @RequestParam(value = "code") String code) {
+
+        model.addAttribute("code", code);
 
         return "index";
     }
